93511def8e28155dcc8204ec0ece4d5a7d2d3397
root <root@vps.server.local>
2023-01-29 21:55:38 +0000

    add Dockerfile

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
-container=TMQTTEx
-img=tomkat/tmqttex
+container=EMQTTEx
+img=tomkat/emqttex
 
 docker build -t $img .
 
@@ -10,16 +10,15 @@ docker container rm $container
 
 #ports - host:container
 docker run -d  -p 8084:80 \
-    --net net_18  --ip 172.18.0.3 \
+    --net net_18  --ip 172.18.0.9 \
     --restart always \
     --name $container \
     -e TZ=Europe/Kiev \
-    -e TEST_ENV_VAR='topic1/test/test2' \
     -e SERVER_PORT=80 \
     -e GET_DELAY=10 \
     -e BROKER_IP='172.18.0.2' \
     -e BROKER_PORT=1883 \
-    -e TOPIC='tele/7C9EBDFA21A0/SENSOR' \
+    -e TOPIC='esphome/meteo1/sensor/#' \
     -e USERNAME='mqtt' \
     -e PASSWORD='mqtt001' \
        $img
